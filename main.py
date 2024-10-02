707ecd348320988aa1842147d4baab98e439590c
Jaeho Jeon <88sprout@gmail.com>
2024-10-02 15:13:11 +0900

    FIX input and print : school->year

---
+++ main.py
@@ -0,0 +1,16 @@
+print("이름을 입력하세요",end=': ')
+name = input()
+print("학번을 입력하세요",end=': ')
+sid = input()
+print("학과를 입력하세요",end=': ')
+major = input()
+print("학년을 입력하세요",end=': ')
+year = input()
+
+print()
+print("<출력>")
+print(f"이름 : {name}")
+print(f"학번 : {sid}")
+print(f"학과 : {major}")
+print(f"학년 : {year}")
+
